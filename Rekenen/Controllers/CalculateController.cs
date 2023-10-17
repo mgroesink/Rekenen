e41669eb3a5092ea1b38977ae7c6207f052189fb
Marcel Roesink <RSK01@rocvantwente.nl>
2023-10-17 12:06:12 +0200

    Add project files.

---
+++ Rekenen/Controllers/CalculateController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rekenen.Models;
+
+namespace Rekenen.Controllers
+{
+    public class CalculateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Calculation());
+        }
+
+        [HttpPost]
+        public IActionResult Index(string option, string category , string level, Calculation calculation,string answer = "") 
+        { 
+            ViewBag.answer = answer;
+            if(option == "Start")
+            {
+                switch(category)
+                {
+                    case "Plus":
+                        calculation = new Calculation(level,'+');
+                        break;
+                    case "Min":
+                        calculation = new Calculation(level , '-');
+                        break;
+                    case "Keer":
+                        calculation = new Calculation(level , 'x');
+                        break;
+                    default:
+                        calculation = null;
+                        break;
+                }
+                ViewBag.answer = "";
+            }
+            if(option == "Check")
+            {
+                if(calculation.Result == int.Parse(answer))
+                {
+                    ViewBag.resultaat = "Jouw antwoord is goed.";
+                    
+                }
+                else
+                {
+                    ViewBag.resultaat = $"Jouw antwoord is helaas niet goed. Het juiste antwoord was {calculation.Result}.";
+                    if(calculation.Operator == '+')
+                    {
+                        ViewBag.resultaat += "<br/>Kijk <a target='blank' href='https://www.beterrekenen.nl/website/index.php?pag=213'>hier</a> voor uitleg";
+                    }
+                    else if(calculation.Operator == '-')
+                    {
+                        ViewBag.resultaat += "<br/>Kijk <a target='blank' href='https://www.beterrekenen.nl/website/index.php?pag=214'>hier</a> voor uitleg";
+
+                    }
+                    else if (calculation.Operator == 'x')
+                    {
+                        ViewBag.resultaat += "<br/>Kijk <a target='blank' href='https://www.beterrekenen.nl/website/index.php?pag=215'>hier</a> voor uitleg";
+                    }
+                }
+            }
+            ViewBag.som = calculation;
+
+            return View(calculation);
+        }
+    }
+}
---
+++ Rekenen/Models/Calculation.cs
@@ -0,0 +1,81 @@
+﻿namespace Rekenen.Models
+{
+    public class Calculation
+    {
+        public int Num1 { get; set; } = 0;
+        public int Num2 { get; set; } = 0;
+        public char Operator { get; set; } = '+';
+
+        public Calculation(int num1 = 0, int num2 = 0, char @operator = '+')
+        {
+            Num1 = num1;
+            Num2 = num2;
+            Operator = @operator;
+        }
+
+        public Calculation()
+        {
+
+        }
+
+        public Calculation(string level = "Simple" , char operation = '+')
+        {
+            Random rnd = new Random();
+            switch(level)
+            {
+                case "Simple":
+                    Num1 = rnd.Next(1,11);
+                    Num2 = rnd.Next(1,11);
+                    Operator = operation;
+                    break;
+                case "Average":
+                    Num1 = rnd.Next(1, 101);
+                    Num2 = rnd.Next(1, 101);
+                    Operator = operation;
+                    break;
+                case "Hard":
+                    Num1 = rnd.Next(-100, 101);
+                    Num2 = rnd.Next(-100, 101);
+                    Operator = operation;
+                    break;
+            }
+        }
+
+        public int Result
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case '+':
+                        return Num1 + Num2;
+
+                    case '-':
+                        return Num1 - Num2; ;
+
+                    case 'x':
+                        return Num1 * Num2;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (Operator)
+            {
+                case '+':
+                    return $"{Num1} + {Num2} = ";
+
+                case '-':
+                    return $"{Num1} - {Num2} = ";
+
+                case 'x':
+                    return $"{Num1} x {Num2} = ";
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
